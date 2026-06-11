e992a601d19ac383fce3069353ab2ac3df341885
shahram17868-cyber <shahram.17868@gmail.com>
2026-06-11 09:03:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("توکنت_رو_اینجا_بذار")
+bot = telebot.TeleBot("8818489067:AAFOY49RuH0LWvjPi3HHlpftvR5OlTmj1b8")
 
 @bot.message_handler(func=lambda m: True)
 def reply(message):
